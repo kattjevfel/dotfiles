ee6b397b9bcad54b59ae3778d7f8a82732dc3d1e
Magnus Boman <magunasu.b97@gmail.com>
2021-11-14 15:15:11 +0100

    remove plasma env stuff (didn't work as I planned)

--- .config/plasma-workspace/env/wayland-stuff.sh
+++ .config/plasma-workspace/env/wayland-stuff.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-    xrandr --output XWAYLAND0 --primary &
-    export QT_QPA_PLATFORM=wayland &
-    export MOZ_ENABLE_WAYLAND=1 &
+    xrandr --output XWAYLAND0 --primary
 fi
