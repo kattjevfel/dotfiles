72fec1e3966f19c8814f9b5ee1a7853bcafa2987
Magnus Boman <magunasu.b97@gmail.com>
2024-02-29 12:41:15 +0100

    un7d: use 7z if 7zz is not available

--- .zsh/functions/un7d.sh
+++ .zsh/functions/un7d.sh
@@ -1,6 +1,12 @@
 un7d() {
+    if [ -f /usr/bin/7zz ]; then
+        sevenzipprog=7zz
+    else
+        sevenzipprog=7z
+    fi
+
     if [[ $# != 1 ]]; then echo I need a single argument, the name of the archive to extract; return 1; fi
     target="${1%.*}"
-    7zz x "$1" -o"${target##*/}"
-    #7zz x "$1" -o"${1%.*}"
+    $sevenzipprog x "$1" -o"${target##*/}"
+    #$sevenzipprog x "$1" -o"${1%.*}"
 }
